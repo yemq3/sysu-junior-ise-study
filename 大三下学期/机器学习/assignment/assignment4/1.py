f0b6cc3424b60f67d5bdd264d3a442eb5ddda43d
“yemq3” <ymqgmaail@gmail.com>
2020-06-09 22:15:34 +0800

    update

--- 大三下学期/机器学习/assignment/assignment4/1.py
+++ 大三下学期/机器学习/assignment/assignment4/1.py
@@ -2,31 +2,43 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 class SVM:
     def __init__(self):
-        self.w = 1
-        self.b = 0
-        self.support_vector = []
+        self.w = None
+        self.b = None
+        self.C = 1
 
     def fit(self, x, y):   
-        self.x = x
-        self.y = y
-        pass
+        self.x = np.matrix(x)
+        self.y = np.matrix(y).T
+        m, n = np.shape(x)
+        self.alpha = np.matrix(np.zeros((m,1))) # 拉格朗日乘子
+        self.simplified_smo()
+        self.w = self.computeW()
+        print(self.w, self.b)
 
     def predict(self, x):
         return np.sign(np.dot(self.w, np.array(x)) + self.b)
 
     def visualize(self):
-        for x, y in zip(self.x, self.y):
-            if y == 1:
-                plt.scatter(x[0], x[1], marker="+", c="r")
+        m, n = np.shape(self.x)
+        for i in range(m):
+            if self.y[i] == 1:
+                plt.scatter(self.x[i,0], self.x[i,1], marker="+", c="r")
             else:
-                plt.scatter(x[0], x[1], marker="_", c="b")
+                plt.scatter(self.x[i,0], self.x[i,1], marker="_", c="b")
 
-        x = np.linspace(min(self.x[:,0])-1,max(self.x[:,1])+1)
-        y = self.w * x + self.b
-        plt.plot(x, y)
+        x1 = min(self.x[:,0])-1
+        x2 = max(self.x[:,1])+1
+        x1, x2 = x1[0,0], x2[0,0]
+        a1, a2 = self.w
+        a1, a2 = a1[0], a2[0]
+        b = self.b
+        y1, y2 = (-b-a1*x1)/a2,(-b-a1*x2)/a2
+        y1, y2 = y1[0,0], y2[0,0]
+        plt.plot([x1,x2], [y1,y2])
 
         plt.xlabel('x')
         plt.ylabel('y')
@@ -35,6 +47,84 @@ class SVM:
 
     def _geometric_margin(self, x):
         return (np.dot(self.w, np.array(x)) + self.b) / np.linalg.norm(self.w)
+
+    def selectJ(self, i, m):
+        # 选定alpha_i后随机选定另外一个alpha_j
+        j = i
+        while j==i:
+            j = random.randint(0,m-1)
+        return j
+
+    def findBounds(self,i,j):
+        if self.y[i] != self.y[j]:
+            L = max(0,self.alpha[j]-self.alpha[i])
+            H = min(self.C, self.C+self.alpha[j]-self.alpha[i])
+        else:
+            L = max(0,self.alpha[i]+self.alpha[j]-self.C)
+            H = min(self.C,self.alpha[i]+self.alpha[j])
+        return L,H
+
+    def clipAlpha(self, j, L, H):
+        if self.alpha[j] > H:
+            self.alpha[j] = H
+        elif self.alpha[j] < L:
+            self.alpha[j] = L
+
+    def kernel(self, i, j):
+        return self.x[i,:] * self.x[j,:].T
+
+    def simplified_smo(self):
+        iters = 0
+        max_iter = 100
+        b = 0
+        m, n = np.shape(self.x)
+        tol = 1e-3
+        while iters < max_iter:
+            num_changed_alphas = 0
+            for i in range(m):
+                ei = self.computeEi(i, b)
+                if (self.y[i] * ei < -tol and self.alpha[i] < self.C) or (self.y[i] * ei > tol and self.alpha[i]> 0 ):
+                    j = self.selectJ(i, m)
+                    ej = self.computeEi(j, b)
+                    old_alphai = self.alpha[i].copy()
+                    old_alphaj = self.alpha[j].copy()
+                    L, H = self.findBounds(i, j)
+                    if L == H:
+                        continue
+                    eta = 2 * self.kernel(i,j) - self.kernel(i,i) - self.kernel(j,j)
+                    if eta >= 0:
+                        continue
+                    self.alpha[j] = self.alpha[j] - (self.y[j] * (ei - ej)) / eta
+                    self.clipAlpha(j, L, H)
+                    if abs(self.alpha[j] - old_alphaj) < 1e-5:
+                        continue
+                    self.alpha[i] = self.alpha[i] + self.y[i] * self.y[j] * (old_alphaj - self.alpha[j])
+                    b1 = b - ei - self.y[i] * (self.alpha[i] - old_alphai) * self.kernel(i,i) - self.y[j] * (self.alpha[j] - old_alphaj) * self.kernel(j,j)
+                    b2 = b - ej - self.y[i] * (self.alpha[i] - old_alphai) * self.kernel(i,j) - self.y[j] * (self.alpha[j] - old_alphaj) * self.kernel(j,j)
+                    if 0 < self.alpha[i] < self.C:
+                        b = b1
+                    elif 0 < self.alpha[j] < self.C:
+                        b = b2
+                    else:
+                        b = (b1+b2)/2
+                    num_changed_alphas += 1
+            if num_changed_alphas == 0:
+                iters += 1
+            else:
+                iters = 0
+        self.b = b
+
+    def computeEi(self, i, b):
+        fxi = np.multiply(self.alpha, self.y).T * (self.x*self.x[i,:].T) + b
+        ei = fxi - self.y[i]
+        return ei
+
+    def computeW(self):
+        m,n = np.shape(self.x)
+        w = np.zeros((n,1))
+        for i in range(m):
+            w += np.multiply(self.alpha[i]*self.y[i], self.x[i,:].T)
+        return w
 
 
 
