af8cd009f9668e52b68f23b4877a2ef4d256b6e4
yemq3 <yemq3@mail2.sysu.edu.cn>
2020-06-09 23:32:00 +0800

    update

--- 大三下学期/机器学习/assignment/assignment4/2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn.svm import SVC
-
-X = np.array([[1,2], [2,3], [3,3], [2,1], [3,2]])
-Y = np.array([1, 1, 1, -1, -1])
-
-clf = SVC()
-clf.fit(X, Y)
-print(clf.support_vectors_)
