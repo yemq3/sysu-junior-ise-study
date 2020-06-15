8ec124589a3b147857c3475948f168e0969f17c3
yemq3 <yemq3@mail2.sysu.edu.cn>
2020-06-15 18:01:03 +0800

    update

--- 大三下学期/机器学习/assignment/assignment4/2.py
+++ 大三下学期/机器学习/assignment/assignment4/2.py
@@ -7,14 +7,15 @@ class SVM:
         self.b = None
         self.C = C
         self.e = []
-        self.kernel = kernel
+        self.kernelType = kernel
         self.alpha = []
+        self.Ecache = []
 
     def fit(self, x, y):   
         self.x = np.matrix(x)
         self.y = np.matrix(y).T
         self.m, self.n = np.shape(x)
-        self.alpha = np.matrix(np.zeros((m,1))) # 拉格朗日乘子
+        self.alpha = np.matrix(np.zeros((self.m,1))) # 拉格朗日乘子
         self.smo()
         self.w = self.computeW()
     
@@ -35,7 +36,7 @@ class SVM:
         return alpha
     
     def kernel(self, i, j):
-        if self.kernel = "linear":
+        if self.kernelType == "linear":
             return i * j.T
 
     def computeEi(self, i):
@@ -55,12 +56,13 @@ class SVM:
         ei = self.computeEi(i)
         best_value = -1
         for j in range(self.m):
-            if abs(ei-self.computeEi(j,self.b)) > best_value:
+            if abs(ei-self.computeEi(j)) > best_value:
                 best_index = j
-                best_value = abs(ei-self.computeEi(j,self.b))
+                best_value = abs(ei-self.computeEi(j))
         return best_index
     
     def takeStep(self, i1, i2):
+        eps = 1e-3
         if i1 == i2:
             return 0
         alpha1 = self.alpha[i1]
@@ -79,31 +81,61 @@ class SVM:
         k12 = self.kernel(self.x[i1,:],self.x[i2,:])
         k22 = self.kernel(self.x[i2,:],self.x[i2,:])
         eta = k11 + k22 - 2*k12
-        if eta > 0:
-            a2 = alpha2 + y2*(e1-e2)/eta
+        if eta < 0:
+            a2 = alpha2 + y2*(e2-e1)/eta
             a2 = self.clipAlpha(a2, L, H)
         else:
-            
+            c1 = eta/2
+            c2 = y2*(e1-e2) - eta *alpha2
+            Lobj = c1 * L * L + c2 * L
+            Hobj = c1 * H * H + c2 * H
+            if Lobj > Hobj + eps:
+                a2 = L
+            elif Lobj < Hobj - eps:
+                a2 = H
+            else:
+                a2 = alpha2
+        if abs(a2-alpha2) < eps*(a2+alpha2+eps):
+            return 0
+        a1 = alpha1+s*(alpha2-a2)
+        if a1 < 0:
+            a2 = s * a1
+            a1 = 0
+        elif a1 > self.C:
+            t = a1-self.C
+            a2 += s*t
+            a1=C
+        self.alpha[i1] = a1
+        self.alpha[i2] = a2
+        b1 = self.b - e1 - self.y[i1] * (self.alpha[i1] - alpha1) * k11 - self.y[i2] * (self.alpha[i2] - alpha2) * k22
+        b2 = self.b - e2 - self.y[i1] * (self.alpha[i1] - alpha1) * k12 - self.y[i2] * (self.alpha[i2] - alpha2) * k22
+        if 0 < self.alpha[i1] < self.C:
+            self.b = b1
+        elif 0 < self.alpha[i2] < self.C:
+            self.b = b2
+        else:
+            self.b = (b1+b2)/2
+        return 1
             
 
-    def examineExample(self, i2):
+    def examineExample(self, i1):
         tol = 1e-3
-        y2 = self.y[i2]
-        alpha2 = self.alpha[i2]
-        e2 = self.computeEi(i2)
-        r2 = e2 * y2
-        if (r2 < - tol and alpha2 < self.C) or (r2 > tol and alpha2 > 0):
-            if len(self.alpha[self.alpha!=0]) + len(self.alpha[self.alpha!=C]) > 1:
-                i1 = selectJ(i2)
+        y1 = self.y[i1]
+        alpha1 = self.alpha[i1]
+        e1 = self.computeEi(i1)
+        r1 = e1 * y1
+        if (r1 < - tol and alpha1 < self.C) or (r1 > tol and alpha1 > 0):
+            if len(self.alpha[self.alpha!=0]) + len(self.alpha[self.alpha!=self.C]) > 1:
+                i2 = self.selectJ(i1)
                 if self.takeStep(i1,i2):
                     return 1
             for i in range(self.m):
-                if self.alpha[i] != 0 and self.alpha[i] != C:
-                    i1 = i
+                if self.alpha[i] > 0 and self.alpha[i] < self.C:
+                    i2 = i
                     if self.takeStep(i1,i2):
                         return 1
             for i in range(self.m):
-                i1 = i
+                i2 = i
                 if self.takeStep(i1,i2):
                     return 1
         return 0
@@ -111,6 +143,7 @@ class SVM:
     def smo(self):
         numChanged = 0
         examineAll = True
+        self.b = 0
         while numChanged > 0 or examineAll:
             numChanged = 0
             if examineAll:
@@ -118,9 +151,54 @@ class SVM:
                     numChanged += self.examineExample(i)
             else:
                 for i in range(self.m):
-                    if self.alpha[i] != 0 and self.alpha[i] != C:
+                    if self.alpha[i] != 0 and self.alpha[i] != self.C:
                         numChanged += self.examineExample(i)
             if examineAll:
                 examineAll = False
             elif numChanged == 0:
                 examineAll = True
+
+    def visualize(self):
+        m, n = np.shape(self.x)
+        for i in range(m):
+            if self.y[i] == 1:
+                plt.scatter(self.x[i,0], self.x[i,1], marker="+", c="r")
+            else:
+                plt.scatter(self.x[i,0], self.x[i,1], marker="_", c="b")
+
+        # 分离超平面
+        x1 = min(self.x[:,0])-1
+        x2 = max(self.x[:,1])+1
+        x1, x2 = x1[0,0], x2[0,0]
+        a1, a2 = self.w
+        a1, a2 = a1[0], a2[0]
+        b = self.b
+        y1, y2 = (-b-a1*x1)/a2,(-b-a1*x2)/a2
+        y1, y2 = y1[0,0], y2[0,0]
+        plt.plot([x1,x2], [y1,y2])
+
+        # 间隔边界
+        plt.plot([x1,x2], [y1+1/a2,y2+1/a2], '--')
+        plt.plot([x1,x2], [y1-1/a2,y2-1/a2], '--')
+
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.show()
+
+
+X = np.array([[1,2], [2,3], [3,3], [2,1], [3,2]])
+Y = np.array([1, 1, 1, -1, -1])
+
+# hisw = []
+# hisb = []
+# for _ in range(100):
+#     svm = SVM(C=10)
+#     svm.fit(X,Y)
+#     hisb.append(svm.b)
+# print(hisb)
+# print(max(hisb) - min(hisb))
+
+svm = SVM(C=20)
+svm.fit(X,Y)
+svm.visualize()
+print(svm.w,svm.b,svm.alpha)
---
+++ 大三下学期/机器学习/笔记/gmm.md
@@ -0,0 +1,54 @@
+## 高斯混合模型（gmm）
+
+对于单维变量，给定均值$\mu$和方差$\sigma$，高斯分布的概率密度函数为
+$$
+f\left(x | \mu, \sigma^{2}\right)=\frac{1}{\sqrt{2 \pi \sigma^{2}}} e^{-\frac{(x-\mu)^{2}}{2 \sigma^{2}}}
+$$
+对于多维变量，$\mu$为均值，$\Sigma$为协方差，$d$为维数
+$$
+\mathcal{N}(x | \mu, \Sigma)=\frac{\exp \left(-\frac{1}{2}(x-\mu)^{\top} \Sigma^{-1}(x-\mu)\right)}{\sqrt{(2 \pi)^{d}|\Sigma|}}
+$$
+高斯混合模型就是k个单一高斯模型的加权求和，即，其中$w_j$为权重，权重之和为1
+$$
+p(x)=\sum_{j=1}^{K}w_{j} \cdot \mathcal{N}\left(x | \mu_{j}, \Sigma_{j}\right)
+$$
+
+### Data generation
+
+给定一个高斯混合模型，可以按照如下步骤生成数据
+
+1. 根据先验分布（$w_j$）选择高斯混合成分j
+2. 根据高斯混合成分j的概率密度函数进行采样
+
+### 训练过程
+
+利用MLE找出$\theta$最大化概率函数$P(x|\theta)$
+$$
+\theta^{*}=\arg \max _{\theta} P(X | \theta)=\arg \max _{\theta} \prod_{i=1}^{N} p\left(x_{i} | \theta\right)
+$$
+![image-20200615165420524](gmm.assets/image-20200615165420524.png)
+
+- $z^{(i)}$为样本$x^{(i)}$由第j个成分生成
+- $w_j$为先验概率
+- $P(x^{(i)}|z^{(i)}=j;\theta)$为条件概率
+- $P(x^{(i)}|\theta)$为全概率
+
+最大化以下似然函数
+$$
+l(\theta)=\sum_{i=1}^{N} \log \mathbb{P}\left(x^{(i)} ; \theta\right)=\sum_{i=1}^{N} \log \sum_{j} \mathbb{P}\left(x^{(i)}, z^{(i)}=j ; \theta\right)
+$$
+即
+$$
+\begin{array}{l}
+\operatorname{argmax} l(\theta)=\operatorname{argmax} \sum_{i=1}^{N} \log \sum_{j=1}^{K} \mathbb{P}\left(z^{(i)}=j\right) \cdot \mathbb{P}\left(x^{(i)} | z^{(i)}=j ; \theta\right) \\
+=\operatorname{argmax} \sum_{i=1}^{N} \log \sum_{j=1}^{K} w_{j} \cdot \mathcal{N}\left(x^{(i)} | \mu_{j}, \Sigma_{j}\right)
+\end{array}
+$$
+![image-20200615172620387](gmm.assets/image-20200615172620387.png)
+
+计算后验概率
+
+![image-20200615174118488](gmm.assets/image-20200615174118488.png)
+
+![image-20200615175721663](gmm.assets/image-20200615175721663.png)
+
