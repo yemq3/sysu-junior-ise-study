92c31909ac2b5954d38827f9dc6a286636e5bdfd
yemq3 <yemq3@mail2.sysu.edu.cn>
2020-06-30 16:49:03 +0800

    update

---
+++ 大三下学期/系统优化/assignment/期末/q1.m
@@ -0,0 +1,79 @@
+% 单纯形法
+
+epsilon = 0.1;
+
+% 扩张系数
+alpha = 1.1;
+
+% 收缩系数
+beta = 0.5;
+
+x1 = [0;0];
+
+x2 = [0;1];
+
+x3 = [1;0];
+
+i = 1;
+
+x = [x1,x2,x3];
+
+disp(x)
+fprintf("----------------------------------------------\n")
+
+while 1
+    x = sortByf(x);
+    old = f(x(:,1));
+    middle_point = (x(:,1) + x(:,2)) / 2;
+    reflect_point = middle_point + (middle_point - x(:,3));
+    if f(reflect_point) < f(x(:,1))
+        % 小于最优点，扩张
+        extern_point = middle_point + alpha * (middle_point - x(:,3));
+        if f(extern_point) < f(reflect_point)
+            x(:,3) = extern_point;
+        else
+            x(:,3) = reflect_point;
+        end
+    elseif f(reflect_point) > f(x(:,1)) && f(reflect_point) < f(x(:,2))
+        % 位于最优点和次优点之间，直接替代
+        x(:,3) = reflect_point;
+    elseif f(reflect_point) > f(x(:,2)) && f(reflect_point) < f(x(:,3))
+        % 位于次优点和最差点之间，收缩
+        shrink_point = middle_point + beta * (reflect_point - middle_point);
+        x(:,3) = shrink_point;
+    else
+        % 比最差点还要差，压缩
+        compress_point = middle_point - beta * (middle_point - x(:,3));
+        if f(compress_point) < f(x(:,3))
+            % 压缩点小于最差点
+            x(:,3) = compress_point;
+        else
+            % 压缩点依旧大于最差点，压缩原三角形
+            x(:,2) = (x(:,1) + x(:,2)) / 2;
+            x(:,3) = (x(:,1) + x(:,3)) / 2;
+        end
+    end
+    
+    x = sortByf(x);
+    disp(x)
+    fprintf("----------------------------------------------\n");
+    i = i + 1;
+    % 避免除0，分母加一个很小的数
+    if abs((f(x(:,3)) - f(x(:,1))) / (f(x(:,1) + 1e-6))) <= epsilon 
+        break
+    end
+end
+
+        
+function y = f(x)
+    y=(x(1)-5)^2+(x(2)-6)^2;
+end
+
+function x = sortByf(x)
+    y1 = f(x(:,1));
+    y2 = f(x(:,2));
+    y3 = f(x(:,3));
+    value=[y1,y2,y3];
+    [~, sortIdx] = sort(value);
+    x = x(:,sortIdx);
+end
---
+++ 大三下学期/系统优化/assignment/期末/q2.m
@@ -0,0 +1,118 @@
+% 遗传算法
+% 设精度为1e-4,则x,y的解空间被60000等分，需要16位二进制数进行编码
+
+N = 100; % 种群大小
+iter = 200; % 最大迭代次数
+crossover_rate = 0.5; % 交配概率
+mutation_rate = 0.001; % 变异概率
+encode_l = 16; % 编码长度
+parent_number = ceil(N*crossover_rate); % 繁殖个体数量
+child_number = N - parent_number; % 替代个体数量
+sum_p = cumsum(ones(parent_number,1)/parent_number); % 累加选择概率
+
+fitness = ones(N, 1);
+
+% 存储每一代的最优值
+best_point = zeros(iter, 2);
+best_fitness = zeros(iter, 1);
+
+% 随机初始化
+population = -3 + 6*rand(N,2);
+
+for i = 1:iter
+    fitness = calaFitness(population);
+    
+    % 大到小排序
+    [fitness, index] = sort(fitness, 'descend');
+    
+    % 保存最优个体数据
+    best_fitness(i) = fitness(1);
+    best_point(i,:) = population(index(1), :);
+    
+    % 选择交配个体
+    parent = population(index(1:parent_number), :);
+    
+    % 交叉繁殖
+    for child = 1:2:child_number
+        mother_index = ceil(rand * parent_number);
+        father_index = ceil(rand * parent_number);
+        % 避免自交
+        while father_index == mother_index
+            father_index = ceil(rand * parent_number);
+        end
+        mother = parent(mother_index, :);
+        father = parent(father_index, :);
+        
+        bin_mother = encode(mother);
+        bin_father = encode(father);
+        
+     
+        % x,y分别更新
+        child1_chrom = ["",""];
+        child2_chrom = ["",""];
+        for chrom = 1:2
+            % 随机选个交叉点
+            crossover_point = randi([2,14],1);
+            
+            mother_chrom = bin_mother(chrom, :);
+            father_chrom = bin_father(chrom, :);
+            
+            mother_part1 = mother_chrom(1:crossover_point);
+            mother_part2 = mother_chrom(crossover_point+1:encode_l);
+            
+            father_part1 = father_chrom(1:crossover_point);
+            father_part2 = father_chrom(crossover_point+1:encode_l);
+            
+            child1_chrom(chrom) = [mother_part1, father_part2];
+            child2_chrom(chrom) = [father_part1, mother_part2];
+        end
+        child1 = decode(child1_chrom);
+        child2 = decode(child2_chrom);
+        population(parent_number + child, :) = child1;
+        population(parent_number + child + 1, :) = child2;
+    end
+    
+    % 变异
+    mutation_number = ceil(N * 2 * encode_l * mutation_rate);
+    for j = 1:mutation_number
+        % 随机选一个个体中的一个染色体的一个位置
+        mutation_unit_index = randi([parent_number,N],1);
+        mutation_chrom_index = randi([1,2],1);
+        mutation_position = randi([1,16],1);
+        % 翻转
+        temp = encode(population(mutation_unit_index ,mutation_chrom_index));
+        if temp(mutation_position) == "0"
+            temp(mutation_position) = "1";
+        else
+            temp(mutation_position) = "0";
+        end
+        temp = decode(temp);
+        population(mutation_unit_index ,mutation_chrom_index) = temp;
+    end
+end
+
+disp(best_point(iter-9:iter,:))
+disp(best_fitness(iter-9:iter))
+
+% 二进制解码
+function x = decode(x)
+    x = -3+bin2dec(x)*6/(2^16-1);
+end
+
+% 编码到二进制
+function x = encode(x)
+    x = (x + 3) / (6 / 2^16);
+    x = dec2bin(x, 16);
+end
+
+function fitness = calaFitness(population)
+    N = size(population, 1);
+    fitness = zeros(N,1);
+    for i = 1:N
+        fitness(i) = f(population(i,:));
+    end
+end
+
+function y = f(x)
+    y = 3 * (1 - x(1))^2 * exp(-x(1)^2 - (x(2) + 1)^2) - 10 * (x(1) / 5 - x(1)^3 - x(2)^5) * exp(-x(1)^2 - x(2)^2) - exp(-(x(1)+1)^2 - x(2)^2) / 3;
+end
